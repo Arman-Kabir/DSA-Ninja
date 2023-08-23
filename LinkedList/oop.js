9146a0d69d143ccf7d8f97c89d58ea63b9ac6dec
Arman-Kabir <armankabir8@gmail.com>
2023-08-23 19:05:29 +0600

    basic oop

---
+++ LinkedList/oop.js
@@ -0,0 +1,19 @@
+class Person {
+    constructor(leg,arm){
+        this.leg = leg
+        this.arm = arm
+    }
+    walk(){
+        console.log('is walking');
+    }
+    // properties
+    leg = 2;
+    arm = 2;
+
+    //methods
+}
+
+const samim = new Person(2, 2);
+const samima = new Person(2, 3);
+
+console.log(samima);
