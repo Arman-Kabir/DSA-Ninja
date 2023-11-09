412c43c0d20b1f1d39fee85e6118d24a1cbb1a69
Arman-Kabir <armankabir8@gmail.com>
2023-11-09 21:43:37 +0600

    adding item to the end in ll done

--- LinkedList/ll.js
+++ LinkedList/ll.js
@@ -7,14 +7,28 @@ class Node {
 // const newNode = new Node(10);
 // console.log(newNode);
 
-class LinkedList{
-    constructor(value){
+class LinkedList {
+    constructor(value) {
         const newNode = new Node(value);
         this.head = newNode;
         this.tail = newNode;
         this.length = 1;
     }
+    // adding node at the end of the Linked List
+    // time complexity O(1)
+    push(value) {
+        const newNode = new Node(value);
+        if (!this.head) {
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            this.tail.next = newNode;
+            this.tail = newNode;
+        }
+        this.length += 1;
+    }
 }
 
 const linkedList = new LinkedList(10);
+linkedList.push(11);
 console.log(linkedList);
