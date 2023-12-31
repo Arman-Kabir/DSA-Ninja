2f57e1202c421601ce4c3ee3d058813d28172a9c
Arman-Kabir <armankabir8@gmail.com>
2023-12-31 10:25:35 +0600

    LL vs array

--- LinkedList/a_ll.js
+++ LinkedList/a_ll.js
@@ -10,6 +10,11 @@ apples
         8742-->pears
                 3722-->null
 
+#4 ->in LL We need to use while loop as don't usually know how long this list is going to be.
+*Most computers have caching system that make reading from sequential memory that is memory right next to each other -> Faster than reading scattered addresses. 
+*Array items are located right next to each other in computer memory.
+*LL and nodes are actually scattered all over memory. Kind of like hash tables.
+*So, iterating through a LL or traversing through a LL is usually quite a bit slower than iterating through items like an array. However,technically both are O(n).
 */
 
 const basket = ['apples','grapes','pears'];
