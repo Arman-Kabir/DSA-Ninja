18c97ea3aff4d8187c6575c1368c38bb24336ff7
Arman-Kabir <armankabir8@gmail.com>
2023-12-31 11:44:43 +0600

    first LL

--- LinkedList/a_ll.js
+++ LinkedList/a_ll.js
@@ -15,6 +15,49 @@ apples
 *Array items are located right next to each other in computer memory.
 *LL and nodes are actually scattered all over memory. Kind of like hash tables.
 *So, iterating through a LL or traversing through a LL is usually quite a bit slower than iterating through items like an array. However,technically both are O(n).
+
+#5 -> Pointer: It's a reference to another place in memory or another object or another node.
+*JS is garbase collected that is memory is managed automatically. Data that is not pointed by any variable or something -> gets automatically garbase collected and deleted because nothing is pointing to it. 
+
+const obj1 = { a: true };
+const obj2 = obj1;
+console.log(obj1);
+
+#6->think of node as a bucket of data
+*Value can be anything we want and the next property has to be a pointer or a reference to the next node.
+
 */
 
-const basket = ['apples','grapes','pears'];
+let myLinkedList = {
+        head: {
+                value: 10,
+                next: {
+                        value: 5,
+                        next: {
+                                value: 16,
+                                next: null
+                        }
+                }
+        }
+}
+
+class LinkedList{
+        constructor(value){
+                this.head = {
+                        value:value,
+                        next:null
+                }
+                this.tail = this.head;
+                this.length =1;
+        }
+        append(value){
+
+        }
+}
+
+const myLinkedList1 = new LinkedList(10);
+myLinkedList1.append(5);
+myLinkedList1.append(16);
+console.log(myLinkedList1);
+
+
