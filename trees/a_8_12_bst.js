8dc68e545e7b45a620b41d7e4ef8ec0b08985131
Arman-Kabir <armankabir8@gmail.com>
2024-01-10 14:14:01 +0600

    bst insert

--- trees/a_8_12_bst.js
+++ trees/a_8_12_bst.js
@@ -18,14 +18,42 @@ class BinarySearchTree {
     constructor() {
         this.root = null;
     }
-    insert(value){}
-    lookup(value){}
+    insert(value) {
+        const newNode = new Node(value);
+        if (this.root === null) {
+            this.root = newNode;
+        } else {
+            let currentNode = this.root;
+            while (true) {
+                if (value < currentNode.value) {
+                    // left
+                    if (!currentNode.left) {
+                        currentNode.left = newNode;
+                        return this;
+                    }
+                    currentNode = currentNode.left
+                } else {
+                    // right
+                    if (!currentNode.right) {
+                        currentNode.right = newNode;
+                        return this;
+                    }
+                    currentNode = currentNode.right;
+                }
+            }
+        }
+    }
+    lookup(value) { }
     // remove
 }
 
 const tree = new BinarySearchTree();
 tree.insert(9);
 tree.insert(4);
+tree.insert(6);
+tree.insert(20);
+tree.insert(415);
+tree.insert(170);
 JSON.stringify()
 
 console.log(tree);
