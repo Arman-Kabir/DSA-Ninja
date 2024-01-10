5147bc2c854e5ffce67210a060b10ecec5e5ddeb
Arman-Kabir <armankabir8@gmail.com>
2024-01-10 10:25:56 +0600

    bst intro

---
+++ trees/a_1_3.js
@@ -0,0 +1,14 @@
+/*
+#1
+
+#2 - Each node can have either 0 1 or 2 nodes and each child can only have one parent.
+
+#5 - Binary Search Tree is good at searching. They are great for comparing things.
+* rules - 1. All child nodes in the tree to the right of the root node must be greater than the current node. If we keep going to the right the number or the value of the node constantly increases.
+2. A node can only have up to 2 children cz it's a binary tree
+*lookup-O(log N), insert-O(log N), delete- O(log N)
+
+#6- 
+
+
+*/
