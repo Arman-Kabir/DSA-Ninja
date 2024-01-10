623a4265e0371f7a4ec2dbe31d8869eb52588cd2
Arman-Kabir <armankabir8@gmail.com>
2024-01-10 13:41:49 +0600

    bst 1st node

---
+++ trees/a_8_12_bst.js
@@ -0,0 +1,31 @@
+/*
+#8
+            9
+    4           20
+1       6   15      170
+
+
+*/
+class Node {
+    constructor(value) {
+        this.left = null;
+        this.right = null;
+        this.value = value;
+    }
+}
+
+class BinarySearchTree {
+    constructor() {
+        this.root = null;
+    }
+    insert(value){}
+    lookup(value){}
+    // remove
+}
+
+const tree = new BinarySearchTree();
+tree.insert(9);
+tree.insert(4);
+JSON.stringify()
+
+console.log(tree);
