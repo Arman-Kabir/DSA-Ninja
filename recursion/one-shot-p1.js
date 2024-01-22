5890387c3c22eb61803f144e4b735a565ac2aca9
Arman-Kabir <armankabir8@gmail.com>
2024-01-22 07:51:48 +0600

    recursion intro

---
+++ recursion/one-shot-p1.js
@@ -0,0 +1,22 @@
+/*
+    prerequisite:
+    Iteration/Loops
+    Functions
+    *Recursion and Iteration vai-vai
+    *Tasks that are done by iteration- all tasks can be done using recursion.
+    Trees , Graphs algorithms use Recursion 
+
+    *When recursion happens inside memory - all the function calls is saved in stack form....When in memory new stack layer is created, As many parameters inside function - new memory space is created for all parameters - in every function call.
+
+    *If we don't keep base case in recursion- in one point our memory gets full and stack overflow problem occurs.
+
+    *We don't use the same variable like iteration in recursion.
+*/
+
+function printNumb(n) {
+    if (n == 0) return;
+    console.log(n);
+    printNumb(n - 1);
+}
+
+printNumb(5);
