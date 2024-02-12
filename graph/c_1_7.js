b3abb6161b30fa496328ab1952670deebf632439
Arman-Kabir <armankabir8@gmail.com>
2024-02-12 10:43:28 +0600

    addEdge,removeEdge done

--- graph/c_1_7.js
+++ graph/c_1_7.js
@@ -17,11 +17,30 @@ class Graph {
     }
     addVertex(vertex) {
         if (!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
+    };
+    addEdge(v1, v2) {
+        this.adjacencyList[v1].push(v2);
+        this.adjacencyList[v2].push(v1);
+    };
+    removeEdge(vertex1, vertex2) {
+        this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter(
+            v => v !== vertex2
+        );
+        this.adjacencyList[vertex2] = this.adjacencyList[vertex2].filter(
+            v => v !== vertex1
+        );
     }
+
 }
 
 const g = new Graph();
 g.addVertex('tokyo');
-g.addVertex(14);
-g.adjacencyList["tokyo"].push('kochi');
+g.addVertex('dallas');
+g.addVertex('aspen');
+g.addVertex('los angeles');
+g.addVertex('hong kong');
+g.addEdge('tokyo', 'dallas');
+g.addEdge('dallas', 'aspen');
+g.removeEdge('tokyo','dallas');
+g.removeEdge('dallas','aspen');
 console.log(g);
