34318ccba31e0a21c2dc6d742fc35024e5126920
Arman-Kabir <armankabir8@gmail.com>
2024-02-14 08:24:37 +0600

    graph traversal intro

---
+++ graph-traversal/c_2_5.js
@@ -0,0 +1,22 @@
+/*
+#2
+    Traversal means visiting every single node or vertex in a graph.
+
+    Trees are special subset of a graph.
+    In graph traversal - we need to specify a starting point.
+
+    Graph traversal uses : 
+    *peer to peer networking
+    *web crawlers
+    *FInding "closest"
+    *matched/recommendations
+    *shortest path problems 
+    *GPS Navigation
+    *Solving mazes
+    *AI(SHortest path to win the game)
+
+    #3
+
+    
+
+*/
